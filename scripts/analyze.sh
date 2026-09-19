1e08c76117514105c3f40cded2919a42208d5577
ElenaXu <3317056001@qq.com>
2026-09-20 00:17:30 +0800

    8 Tasks

--- scripts/analyze.sh
+++ scripts/analyze.sh
@@ -8,3 +8,18 @@
 # TODO: print:
 # Total ERROR: <number>
 # Top Code: <code>
+if [ $# -ne 1 ];then
+    echo "Usage: $0 FILE"
+    exit 1
+fi
+
+if [ ! -f "$1" ];then
+    echo "Error:FILE $1 does not exist"
+    exit 1
+fi
+
+ERROR_COUNT=$(grep ERROR $1 | wc -l)
+TOP_CODE=$(grep ERROR $1 | cut -d ' ' -f 5 | cut -d '=' -f 2 | sort | uniq -c | sort -nr | head -n1 | tr -s ' ' | cut -d ' ' -f 3)
+
+echo "Total ERROR: $ERROR_COUNT"
+echo "Top Code: $TOP_CODE"
